923ea9c3b46ca6d706d9d728839af0c1a27db6ee
bzp99 <bpeter@edu.bme.hu>
2026-03-05 11:13:11 +0100

    feat: add naive withdraw feature

--- src/Auction.sol
+++ src/Auction.sol
@@ -4,17 +4,32 @@ pragma solidity ^0.8.20;
 contract Auction {
     address public highestBidder;
     uint256 public highestBid;
+    mapping(address bidder => uint256 bidAmount) public pendingReturns;
 
     event BidPlaced(address indexed bidder, uint256 amount);
 
     error BidTooLow();
+    error NothingToWithdraw();
+    error TransferFailed();
 
     function bid() external payable {
         if (msg.value <= highestBid) revert BidTooLow();
+
+        pendingReturns[highestBidder] = highestBid;
 
         highestBidder = msg.sender;
         highestBid = msg.value;
 
         emit BidPlaced(msg.sender, msg.value);
+    }
+
+    function withdraw() external {
+        uint256 amount = pendingReturns[msg.sender];
+        if (amount == 0) revert NothingToWithdraw();
+
+        (bool success,) = msg.sender.call{value: amount}("");
+        if (!success) revert TransferFailed();
+
+        pendingReturns[msg.sender] = 0;
     }
 }
