57ca43b153b55973269949c1d17df6c10d6ebc03
bzp99 <bpeter@edu.bme.hu>
2026-03-05 11:13:12 +0100

    feat: add access control and auction phases

--- src/Auction.sol
+++ src/Auction.sol
@@ -4,17 +4,43 @@ pragma solidity ^0.8.20;
 import {ReentrancyGuardTransient} from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
 
 contract Auction is ReentrancyGuardTransient {
+    enum AuctionPhase {
+        BIDDING,
+        ENDED
+    }
+
+    address public owner;
+    AuctionPhase public phase;
+
     address public highestBidder;
     uint256 public highestBid;
+
     mapping(address bidder => uint256 bidAmount) public pendingReturns;
 
     event BidPlaced(address indexed bidder, uint256 amount);
+    event AuctionEnded(address indexed winner, uint256 amount);
 
     error BidTooLow();
     error NothingToWithdraw();
     error TransferFailed();
+    error OnlyOwner();
+    error NotInPhase(AuctionPhase phase);
 
-    function bid() external payable {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert OnlyOwner();
+        _;
+    }
+
+    modifier onlyDuring(AuctionPhase _phase) {
+        if (phase != _phase) revert NotInPhase(_phase);
+        _;
+    }
+
+    function bid() external payable onlyDuring(AuctionPhase.BIDDING) {
         if (msg.value <= highestBid) revert BidTooLow();
 
         pendingReturns[highestBidder] = highestBid;
@@ -33,5 +59,14 @@ contract Auction is ReentrancyGuardTransient {
 
         (bool success,) = msg.sender.call{value: amount}("");
         if (!success) revert TransferFailed();
+    }
+
+    function endAuction() external onlyOwner onlyDuring(AuctionPhase.BIDDING) {
+        phase = AuctionPhase.ENDED;
+
+        (bool success,) = owner.call{value: highestBid}("");
+        if (!success) revert TransferFailed();
+
+        emit AuctionEnded(highestBidder, highestBid);
     }
 }
